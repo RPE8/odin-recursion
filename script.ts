bd6f0965e648e1186368ba5733041ecb324c1609
RPE8 <vitali.shuman96@gmail.com>
2022-10-03 14:06:35 +0300

    Add solution for Merge Sort assigment

--- script.ts
+++ script.ts
@@ -24,3 +24,32 @@ console.log(fibsRec(1));
 console.log(fibsRec(2));
 console.log(fibsRec(8));
 console.log(fibsRec(21));
+
+
+function merge(left: number[], right: number[]): number[] {
+	const result: number[] = [];
+	while (left.length && right.length) {
+		let value: number | undefined;
+		if (left[0] < right[0]) {
+			value = left.shift();
+			if (value) result.push(value);
+		} else {
+			value = right.shift();
+			if (value) result.push(value);
+		}
+	}
+	return [...result, ...left, ...right];
+}
+
+function mergeSort(arr: number[]): number[] {
+	if (arr.length < 2) {
+		return arr;
+	}
+	const middle = Math.floor(arr.length / 2);
+	const left = arr.slice(0, middle);
+	const right = arr.slice(middle);
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort([1, 3, 5, 2, 4, 6]));
+console.log(mergeSort([1, 3, 5]));
